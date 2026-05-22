a0f2a48dfb6ff60b5f8b26efef796f2fc44c3b5f
ayman-abdelghaffar <aymanabdo01206473107@gmail.com>
2026-05-22 12:12:46 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(fetch(e.request).catch(() => {
+        console.log('[Service Worker] Fetch failed');
+    }));
+});
